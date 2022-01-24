e734704e0f026efff42d7b19c2175a31f17e9192
kolace <56091232+kolace@users.noreply.github.com>
2022-01-24 20:56:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -88,4 +88,4 @@ const templateElement=document.getElementById("templateHB");
 const templateSource=templateElement.innerHTML;
 const template=Handlebars.compile(templateSource);
 const compiledHtml=template(context);
-document.getElementById("information").innerHTML=compiledHtml;
+document.getElementById("dictionary").innerHTML=compiledHtml;
